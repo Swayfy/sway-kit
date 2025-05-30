08dbeeced92b408424845bedeb295ca36e13fd6c
dominiq007 <dom.rajkowski@gmail.com>
2025-05-30 20:19:14 +0200

    chore(core): set up package stucture

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,52 @@
+import { builtinModules } from 'node:module';
+import { defineConfig } from 'vite';
+import swc from 'vite-plugin-swc-transform';
+
+export default defineConfig({
+  logLevel: 'error',
+  build: {
+    target: 'esnext',
+    sourcemap: true,
+    lib: {
+      entry: './src/exports.ts',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: [
+        'chalk',
+        ...builtinModules.map((moduleName) => `node:${moduleName}`),
+      ],
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        format: 'es',
+        entryFileNames: '[name].js',
+      },
+    },
+    outDir: './build',
+    assetsDir: './',
+  },
+  plugins: [
+    swc({
+      swcOptions: {
+        minify: true,
+        jsc: {
+          minify: {
+            compress: {
+              unused: true,
+            },
+            mangle: true,
+          },
+          parser: {
+            syntax: 'typescript',
+            decorators: true,
+          },
+          target: 'es2024',
+          transform: {
+            decoratorVersion: '2022-03',
+          },
+        },
+      },
+    }),
+  ],
+});
