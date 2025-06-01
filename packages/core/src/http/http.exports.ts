ebdd086c38ed167aeb1ef92ee7bb5d0f3ec15202
dominiq007 <dom.rajkowski@gmail.com>
2025-06-01 18:32:07 +0200

    fix(core): exports

--- packages/core/src/http/http.exports.ts
+++ packages/core/src/http/http.exports.ts
@@ -2,6 +2,6 @@ export * from './enums/http-method.enum.ts';
 export * from './enums/http-status.enum.ts';
 export * from './http-error.class.ts';
 export * from './json-response.class.ts';
-export * from './view-response.class.ts';
 export * from './request.class.ts';
 export * from './response.class.ts';
+export * from './view-response.class.ts';
