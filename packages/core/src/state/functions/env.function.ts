b31f886b03261fc004e29c43b0947c5d2078ab53
dominiq007 <dom.rajkowski@gmail.com>
2025-06-05 16:37:49 +0200

    feat(core): add env() default value

--- packages/core/src/state/functions/env.function.ts
+++ packages/core/src/state/functions/env.function.ts
@@ -1,11 +1,12 @@
-import { StateManager } from '../state-manager.service.ts';
 import { EnvVariable } from '../../utils/types/env-variable.type.ts';
 import { resolve } from '../../injector/functions/resolve.function.ts';
+import { StateManager } from '../state-manager.service.ts';
 
 const stateManager = resolve(StateManager);
 
 export function env<TValue extends EnvVariable>(
   key: string,
+  defaultValue?: TValue,
 ): TValue | undefined {
-  return stateManager.getEnv<TValue>(key);
+  return stateManager.getEnv<TValue>(key, defaultValue);
 }
